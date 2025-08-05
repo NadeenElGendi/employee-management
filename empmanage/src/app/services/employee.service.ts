55736f7b9c74a16cff1f3bfd27875d5a1940e85b
NadeenElGendi <nadineamrelgendi@gmail.com>
2025-08-06 01:50:55 +0300

    #	modified:   empmanage/src/app/services/employee.service.ts #	new file:   empmanage/src/environments/environment.ts

--- empmanage/src/app/services/employee.service.ts
+++ empmanage/src/app/services/employee.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { Employee, EmployeeViewModel, EditEmployeeViewModel } from '../models/employee.model';
-import { environment } from '../../../src/environments/environment.prod';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -17,66 +17,47 @@ export class EmployeeService {
     })
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   // Get all employees
   getAllEmployees(): Observable<Employee[]> {
     return this.http.get<Employee[]>(`${this.apiUrl}/getAllEmployees`, this.httpOptions)
-      .pipe(
-        retry(1),
-        catchError(this.handleError)
-      );
+      .pipe(retry(1), catchError(this.handleError));
   }
 
   // Get employee by ID
   getEmployeeById(empId: number): Observable<Employee> {
     return this.http.get<Employee>(`${this.apiUrl}/getEmpByID/${empId}`, this.httpOptions)
-      .pipe(
-        retry(1),
-        catchError(this.handleError)
-      );
+      .pipe(retry(1), catchError(this.handleError));
   }
 
   // Add new employee
   addEmployee(employee: EmployeeViewModel): Observable<any> {
     return this.http.post(`${this.apiUrl}/addEmployee`, employee, this.httpOptions)
-      .pipe(
-        retry(1),
-        catchError(this.handleError)
-      );
+      .pipe(retry(1), catchError(this.handleError));
   }
 
   // Edit employee
   editEmployee(employee: EditEmployeeViewModel): Observable<any> {
     return this.http.post(`${this.apiUrl}/editEmployee`, employee, this.httpOptions)
-      .pipe(
-        retry(1),
-        catchError(this.handleError)
-      );
+      .pipe(retry(1), catchError(this.handleError));
   }
 
   // Delete employee
   deleteEmployee(empId: number): Observable<any> {
     return this.http.get(`${this.apiUrl}/deleteEmpByID/${empId}`, this.httpOptions)
-      .pipe(
-        retry(1),
-        catchError(this.handleError)
-      );
+      .pipe(retry(1), catchError(this.handleError));
   }
 
   // Error handling
   private handleError(error: HttpErrorResponse) {
     let errorMessage = 'An error occurred';
-    
     if (error.error instanceof ErrorEvent) {
-      // Client-side error
       errorMessage = `Error: ${error.error.message}`;
     } else {
-      // Server-side error
       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
-    
     console.error(errorMessage);
     return throwError(() => new Error(errorMessage));
   }
-} 
+}
